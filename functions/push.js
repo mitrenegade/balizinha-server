32fc727388c0025fe7e9d0347a3044c4caae7662
Bobby <bobbyren@gmail.com>
2017-12-20 08:35:13 -0500

    migrate balizinha-server from balizinha-ios; version 0.6.0 of iOS currently supported - push notifications for events - payments for games; trial organizers

---
+++ functions/push.js
@@ -0,0 +1,5 @@
+const axios = require('axios');
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+
+// https://aaronczichon.de/2017/03/13/firebase-cloud-functions/
