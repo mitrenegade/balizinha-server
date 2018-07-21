c1d974134a82be75bb06c66c9a9c763786f9414d
bobbyren <bobbyren@gmail.com>
2018-07-21 09:50:37 -0400

    added actionJS and moved action calls

--- functions/index.js
+++ functions/index.js
@@ -412,120 +412,6 @@ exports.getUniqueId = functions.https.onRequest( (req, res) => {
     res.status(200).json({"id": uniqueId})
 })
 
-// actions
-exports.createAction = function(type, userId, eventId, message) {
-    console.log("createAction type: " + type + " event id: " + eventId + " message: " + message)
-    // NOTE: ref url is actions. iOS < v0.7.1 uses /action
-
-    var actionId = exports.createUniqueId()
-
-    var params = {}
-    params["type"] = type
-    params["event"] = eventId
-    params["user"] = userId
-    params["message"] = message
-    var createdAt = exports.secondsSince1970()
-    params["createdAt"] = createdAt
-
-    return admin.database().ref(`/players/${userId}`).once('value').then(snapshot => {
-        return snapshot.val();
-    }).then(player => {
-        var name = player["name"]
-        params["username"] = name
-
-        var ref = `/actions/` + actionId
-        console.log("Creating action in /actions with unique id " + actionId + " message: " + message + " params: " + JSON.stringify(params))
-        return admin.database().ref(ref).set(params)
-    }).then(action => {
-        // create eventAction
-        if (eventId != undefined) {
-            var ref = `/eventActions/` + eventId
-            // when initializing a dict, use [var] notation. otherwise use params[var] = val
-            var params = { [actionId] : true}
-            console.log("Creating eventAction for event " + eventId + " and action " + actionId + " with params " + JSON.stringify(params))
-            return admin.database().ref(ref).update(params)
-        }
-    })
-}
-
-exports.onActionChange = functions.database.ref('/actions/{actionId}').onWrite((snapshot, context) => {
-    const actionId = context.params.actionId
-    var changed = false
-    var created = false
-    var deleted = false
-    var data = snapshot.after.val()
-    var old = snapshot.before
-
-    if (!old.exists()) {
-        created = true
-        console.log("onActionChange: created action " + actionId)
-    } else if (old["active"] == true && data["active"] == false) {
-        deleted = true
-        console.log("onActionChange: deleted action " + actionId)
-    }
-
-    if (!created && !deleted) {
-        changed = true;
-        console.log("onActionChange: changed action " + actionId)
-    }
-
-    const actionType = data["type"]
-    if (actionType == "chat" && created == true) {
-    // for a chat action, update createdAt, username then create a duplicate
-        const createdAt = exports.secondsSince1970()
-        const userId = data["user"]
-        return admin.database().ref(`/players/${userId}`).once('value').then(snapshot => {
-            return snapshot.val();
-        }).then(player => { 
-            // add player username and createdAt
-            var ref = `/actions/` + actionId
-            var name = player["name"]
-            console.log("Action: adding createdAt " + createdAt)
-            return admin.database().ref(ref).update({"createdAt": createdAt, "username": name})
-        }).then(result => {
-            // create the same under /action
-            // TODO: deprecate in ios 0.7.3
-            var legacyref = `/action/` + actionId
-            data["createdAt"] = createdAt
-            console.log("Duplicating action under /action with unique id " + actionId + " message: " + data["message"])
-            return admin.database().ref(legacyref).set(data)
-        }).then(result => {
-            // create eventAction
-            var eventId = data["event"]
-            var ref = `/eventActions/` + eventId
-            // when initializing a dict, use [var] notation. otherwise use params[var] = val
-            var params = { [actionId] : true}
-            console.log("Creating eventAction for event " + eventId + " and action " + actionId + " with params " + JSON.stringify(params))
-            return admin.database().ref(ref).update(params)
-        }).then(result => {
-            // send push
-            console.log("onActionChange: pushForChatAction with result " + JSON.stringify(result))
-            exports.pushForChatAction(actionId, data["event"], data["user"], data)
-            return result
-        })
-    } else {
-        return snapshot
-    }
-});
-
-exports.pushForChatAction = function(actionId, eventId, userId, data) {
-    console.log("push for chat: " + actionId + " event: " + eventId + " user: " + userId + " data: " + JSON.stringify(data))
-
-    var eventTopic = "event" + eventId
-    return admin.database().ref(`/players/${userId}`).once('value').then(snapshot => {
-        return snapshot.val();
-    }).then(player => {
-        var name = player["name"]
-        var email = player["email"]
-        var message = data["message"]
-        var msg = name + " said: " + message
-        var title = "Event chat"
-        var topic = "event" + eventId 
-        console.log("Sending push for chat by user " + name + " " + email + " for chat to topic " + topic + " with message: " + msg)
-
-        return exports.sendPushToTopic(title, topic, msg)
-    })
-}
 
 // Push
 exports.sendPushToTopic = function(title, topic, msg) {
@@ -669,4 +555,13 @@ exports.onEventChange = functions.database.ref('/events/{eventId}').onWrite((sna
 
 exports.onUserJoinOrLeaveEvent = functions.database.ref('/eventUsers/{eventId}/{userId}').onWrite((snapshot, context) => {
     return eventModule.onUserJoinOrLeaveEvent(snapshot, context, exports, admin)
+})
+
+// ACTION //////////////////////////////////////////////////////////////////////////////////
+exports.createAction = function(type, userId, eventId, message) {
+    return actionModule.createAction(type, userId, eventId, message, exports, admin)
+}
+
+exports.onActionChange = functions.database.ref('/actions/{actionId}').onWrite((snapshot, context) => {
+    return actionModule.onActionChange(snapshot, context, exports, admin)
 })
