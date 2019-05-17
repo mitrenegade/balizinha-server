c13890050ff6b875ec8c4d0f642865516bf8d40b
bobbyren <bobbyren@gmail.com>
2019-05-16 23:13:39 -0400

    venue returns error with 500 instead of empty json

--- functions/venue1.0.js
+++ functions/venue1.0.js
@@ -4,7 +4,7 @@ exports.getVenues = function(req, res) {
 	let ref = `/venues/`
 	return admin.database().ref(ref).once('value').then(snapshot => {
 		if (!snapshot.exists()) {
-	 		res.status(200).json({"results": []})
+	 		res.status(500).json({"error": "No venues found"})
 		} else {
 	 		res.status(200).json({"results": snapshot.val()})
 		}
@@ -15,7 +15,7 @@ exports.getCities = function(req, res) {
 	let ref = `/cities/`
 	return admin.database().ref(ref).once('value').then(snapshot => {
 		if (!snapshot.exists()) {
-	 		res.status(200).json({"results": []})
+	 		res.status(500).json({"error": "No cities found"})
 		} else {
 	 		res.status(200).json({"results": snapshot.val()})
 		}
